cbd6a960d7a00f294633a62397de5772adcfa2db
shan kuriakose <shan.kur456@gmail.com>
2022-12-02 13:48:36 +0530

    completed ## final changes

--- day 1/main.py
+++ day 1/main.py
@@ -3,27 +3,22 @@ with open ("data.txt") as file:
 
 ## traversing every item in the data
 
-count = 0
-max = 0
-max1 = 0 #calory carried by the second elf
-max2 = 0 # calory carried by the third elf
+load = 0
+listOfloads = []
+
 
 for item in data :
     if item == "":
-        count = 0
+        listOfloads.append(load)
+        load = 0
     else:
         num = int(item)
-        count += num
-    if count > max:
-        max = count
-    elif count > max1:
-        max1 = count
-    elif count > max2:
-        max2 = count
+        load += num
 
-print(f"anser to part 1: {max}")
-print(f"answer for part 2: {max+max1+max2}")
-print(max,max1,max2, count)
+listOfloads.sort(reverse=True)
+
+print(f"anser to part 1: {listOfloads[0]}")
+print(f"answer for part 2: {listOfloads[0]+listOfloads[1]+listOfloads[2]}")
 
 
 
---
+++ day 1/test.py
@@ -0,0 +1,4 @@
+
+primenumbers = [1,2,34,56,6,6]
+primenumbers.sort(reverse=True)
+print(primenumbers[0])
